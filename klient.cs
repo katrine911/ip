113e57294ed366cb77d274e928941672feb8036b
katrine911 <kate.efremova@gmail.com>
2017-06-26 00:03:33 +0300

    Update klient.cs

--- klient.cs
+++ klient.cs
@@ -1,68 +1,32 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
-public class Udp_Client
+namespace client
 {
-    public static void Main()
+    class Program
     {
-        byte[] data = new byte[1024];
-        string input, stringData;
+        static void Main(string[] args) {
+            while (true) {
+                try {
+                    string filePath = Console.ReadLine();
+                    TcpClient socket = new TcpClient("7.31.197.143", 1255);
+                    NetworkStream nwStream = socket.GetStream();
+                    BinaryReader reader = new BinaryReader(nwStream);
+                    BinaryWriter writer = new BinaryWriter(nwStream);
+                    writer.Write(filePath);
 
-        //Ввод IP-адреса сервера
-        Console.Write("Укажите IP-адрес сервера: ");
-        string addr = Console.ReadLine();
-        //Если адрес пустой - принимаем за локалхост
-        if (addr == "") addr = "127.0.0.1";
-
-        //Создание нового UDP-клиента и установка удаленного узла по умолчанию
-        UdpClient server = new UdpClient(addr, 9050);
-        //создаем конечную точку по адресу сокета. Т.е. будем "слушать" порты 
-        //и контролировать все сетевые интерфейсы
-        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-
-        //Отправляем серверу сообщение о подключении
-        string welcome = "Клиент успешно подключился!";
-        data = Encoding.UTF8.GetBytes(welcome);
-        server.Send(data, data.Length);
-
-        //От сервера придут данные, типа "Подключение успешно"
-        data = server.Receive(ref sender);
-
-        //Обеспечиваем их отображение на экране
-        Console.Write("Сообщение принято от {0}:", sender.ToString());
-        stringData = Encoding.UTF8.GetString(data, 0, data.Length);
-        Console.WriteLine(stringData);
-
-        //Бесконечный цикл.
-        while (true)
-        {
-            //Объявляем новый массив под пришедшие данные.
-            data = new byte[1024];
-            //Ожидаем ввода с клавиатуры (строки) и заносим в переменную input
-            Console.Write("\r\n>");
-            input = Console.ReadLine();
-
-            //Перевод отсылаемой строки в байты
-            data = Encoding.UTF8.GetBytes(input);
-            //Отсылаем серверу строку (переведенную в байты)
-            server.Send(data, data.Length);
-
-            //Если пришла команла exit - выходим из цикла (далее - закрываем сокет и т.д.)
-            if (input == "exit") break;
-
-            //Получение данных...
-            data = server.Receive(ref sender);
-            //Перевод принятых байтов в строку
-            stringData = Encoding.UTF8.GetString(data, 0, data.Length);
-            Console.Write("<");
-            //Отображение на экране принятой строки (размер файла)
-            Console.WriteLine(stringData);
+                    Console.WriteLine(reader.ReadString() + "\n");
+                    socket.Close();
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+            }
+            Console.ReadKey();
         }
-        //После выхода из цикла...
-        Console.WriteLine("Остановка клиента...");
-        //Закрываем UDP-соединение
-        server.Close();
     }
 }
