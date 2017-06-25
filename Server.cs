d10aef5c6f5ebcd747e6589328c1fedbd1b430cb
katrine911 <kate.efremova@gmail.com>
2017-06-26 00:04:41 +0300

    Add files via upload

---
+++ Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_server
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            TcpListener serverSocket = new TcpListener(1255);
+            serverSocket.Start();
+
+            Console.WriteLine("Сервер запущен");
+
+            while (true) {
+                try {
+                    TcpClient socket = serverSocket.AcceptTcpClient();
+                    NetworkStream nwStream = socket.GetStream();
+                    BinaryReader reader = new BinaryReader(nwStream);
+                    BinaryWriter writer = new BinaryWriter(nwStream);
+
+                    string filePath = reader.ReadString();
+
+                    Console.WriteLine("Запрошена информация о файле " + filePath);
+                    FileInfo file = new FileInfo(filePath);
+
+                    string response = file.Exists ? file.FullName + ":\nДата создания: " + file.CreationTime + "\nРазмер: " + file.Length + " байт" : "Файл не найден";
+                    writer.Write(response);
+                    socket.Close();
+                } catch(Exception e) { Console.WriteLine(e.Message); }
+            }
+            serverSocket.Stop();
+            Console.ReadKey();
+        }
+    }
+}
